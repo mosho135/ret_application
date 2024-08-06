470ef681a71438b601db33f2c40fbfe650aed153
Marshall <marshall1@live.co.za>
2024-08-06 17:13:28 +0200

    UPDATE - Added password

--- app.py
+++ app.py
@@ -5,9 +5,43 @@ import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 import datetime as dt
+import hmac
 
 today = dt.datetime.today()
 today = today.strftime('%Y%m%d%H%M%S')
+
+# Password check
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# Main Streamlit app starts here
+st.write("Here goes your normal Streamlit app...")
+st.button("Click me")
 
 # TODO: add in the areas for the map view. And look to deploy and web application and test
 # TODO: Add a download button for the HTML map for quicker loading time
