44c5883bd8ba649d96e8900c69436600b86cda25
Marshall Raubenheimer <marshall1@live.co.za>
2026-04-14 06:47:17 +0200

    Removed st write for the shape of the df

--- app.py
+++ app.py
@@ -472,7 +472,6 @@ elif selected=='WC Inactive Customers':
     )
 elif selected=='GCM Active Customers':
     df = get_data('GA')
-    st.write(df.shape)
     df_selection = side_filter_selection(df)
     metrics(df_selection)
     veiw_filter = st.radio(
